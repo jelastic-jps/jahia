560c4a007e223df24a72bbed2fd3f9857f996a49
DmytroZubelevych <31444413+DmytroZubelevych@users.noreply.github.com>
2020-01-13 12:27:00 +0200

    Publish of JE-50131

--- scripts/setupUser.sh
+++ scripts/setupUser.sh
@@ -1,29 +1,37 @@
-#!/bin/bash
-
-user=$1
-pswd=$2
-
+#!/bin/bash                                                                                                                                                                                                                                                                    
+                                                                                                                                                                                                                                                                               
+user=$1                                                                                                                                                                                                                                                                        
+pswd=$2                                                                                                                                                                                                                                                                        
+ADMIN_PASSWORD=$(pwgen 10 1)                                                                                                                                                                                                                                                   
+JEM=$(which jem)
+MYSQL=$(which mysql)
+MYSQLD_SAFE=$(which mysqld_safe)
 unset resp;
+version=$(mysql --version|awk '{ print $5 }'|awk -F\, '{ print $1 }')
 resp=$(mysql -u$user -p$pswd mysql --execute="SHOW COLUMNS FROM user")
 [ -z "$resp" ] && {
-        service mysql stop;
-        mysqld_safe --skip-grant-tables --user=mysql --pid-file=/var/lib/mysql/$(hostname).pid &
-        sleep 5
-        cmd="CREATE TEMPORARY TABLE tmptable SELECT * FROM user WHERE User = 'root'; UPDATE tmptable SET User = '$user' WHERE User = 'root'; DELETE FROM user WHERE User = '$user'; INSERT INTO user SELECT * FROM tmptable WHERE User = '$user'; DROP TABLE tmptable;"
-        mysql mysql --execute="$cmd"
-
-        version=$(mysql --version|awk '{ print $5 }'|awk -F\, '{ print $1 }')
-
-        if (( $(awk 'BEGIN {print ("'$version'" >= "'5.7'")}') )); then
-                cmd="UPDATE user SET authentication_string=PASSWORD('$pswd') WHERE user='$user';";
+        if (( $(awk 'BEGIN {print ("'$version'" >= "'10.4'")}') )); then
+            $JEM passwd set -p $ADMIN_PASSWORD
+            cmd="CREATE USER '$user'@'localhost' IDENTIFIED BY '$pswd'; CREATE USER '$user'@'%' IDENTIFIED BY '$pswd'; GRANT ALL PRIVILEGES ON *.* TO '$user'@'localhost' WITH GRANT OPTION; GRANT ALL PRIVILEGES ON *.* TO '$user'@'%' WITH GRANT OPTION; FLUSH PRIVILEGES;"
+            $MYSQL -uroot -p${ADMIN_PASSWORD} --execute="$cmd"
         else
-                cmd="UPDATE user SET password=PASSWORD('$pswd') WHERE user='$user';";
-        fi
+            service mysql stop;
+            $MYSQLD_SAFE --skip-grant-tables --user=mysql --pid-file=/var/lib/mysql/$(hostname).pid &
+            sleep 5
+            cmd="CREATE TEMPORARY TABLE tmptable SELECT * FROM user WHERE User = 'root'; UPDATE tmptable SET User = '$user' WHERE User = 'root'; DELETE FROM user WHERE User = '$user'; INSERT INTO user SELECT * FROM tmptable WHERE User = '$user'; DROP TABLE tmptable;"
+            $MYSQL mysql --execute="$cmd"
 
-        mysql mysql --execute="$cmd"
-        echo $resp
-        rm -f /var/lib/mysql/auto.cnf
-        service mysql restart
+            if (( $(awk 'BEGIN {print ("'$version'" >= "'5.7'")}') )); then
+                cmd="UPDATE user SET authentication_string=PASSWORD('$pswd') WHERE user='$user';";
+            else
+                cmd="UPDATE user SET password=PASSWORD('$pswd') WHERE user='$user';";
+            fi
+
+            $MYSQL mysql --execute="$cmd"
+            echo $resp
+            rm -f /var/lib/mysql/auto.cnf
+            service mysql restart
+        fi
 } || { 
         echo "[Info] User $user has the required access to the database." 
 }
